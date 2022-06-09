493bcaf7633635e941092b8ca728c939ceea7a46
Joseph Hooker <jcolehooker@gmail.com>
2022-06-08 22:53:38 -0500

    initial asp.net web app w/ razor set up, some problems with controllers + views rectified

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ajax_demo1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Customer");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace ajax_demo1.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Customer(int iD, string name, int age)
+        {
+            ID = iD;
+            Name = name;
+            Age = age;
+        }
+    }
+}
